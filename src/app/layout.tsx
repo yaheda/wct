72cd11b3e88d1db571e653ee0cd7ff0b41e9fa8a
Abu Yaheda <yaheda.a@gmail.com>
2025-10-07 20:23:54 +0200

    Add Vercel Analytics component to RootLayout for enhanced tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,7 @@ export default function RootLayout({
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
           {children}
+          <Analytics />
         </body>
       </html>
     </ClerkProvider>
